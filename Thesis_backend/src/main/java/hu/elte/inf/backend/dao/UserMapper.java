d9dcbf45c021b47f221715ea66295b5114c95286
Yvette-12 <chensiyyyu12@gmail.com>
2024-04-12 12:09:47 +0200

    update something

--- Thesis_backend/src/main/java/hu/elte/inf/backend/dao/UserMapper.java
+++ Thesis_backend/src/main/java/hu/elte/inf/backend/dao/UserMapper.java
@@ -7,8 +7,30 @@ import org.mybatis.spring.annotation.MapperScan;
 // 数据访问层负责与数据库进行直接交互
 @Mapper
 public interface UserMapper {
+    /**
+     * Insert one line data
+     * @param user
+     * @return the changing line number
+     */
     Integer insertUser(User user);
+
+    /**
+     * Get user by username
+     * @param username
+     * @return User
+     */
     User getUserByUserName(String username);
+
+    /**
+     * Get user by email
+     * @param email
+     * @return user
+     */
     User getUserByEmail(String email);
+
+    /**
+     * Update user information
+     * @param user
+     */
     void updateUserInfo(User user);
 }
