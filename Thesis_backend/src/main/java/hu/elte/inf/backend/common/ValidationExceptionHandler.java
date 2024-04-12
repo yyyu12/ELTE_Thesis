282d57a1e4171db09062359da6c079169461bb7b
Guohao Lin <gouhao.lin@tum.de>
2024-04-12 17:01:13 +0200

    Add verification exception handling

---
+++ Thesis_backend/src/main/java/hu/elte/inf/backend/common/ValidationExceptionHandler.java
@@ -0,0 +1,31 @@
+package hu.elte.inf.backend.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Result handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        Result result = Result.error(HttpStatus.BAD_REQUEST.value(), "Validation failed");
+        result.put("errors", errors);
+        return result;
+    }
+}
